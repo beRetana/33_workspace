5e3863e407141a47b2b71ea0f43339793fa228cb
Brandon Eduardo Retana Garcia <beretana@uci.edu>
2026-04-05 13:38:01 -0700

    restructure

--- Reinforcement_Exercises/RE_Eight/problem1.py
+++
@@ -1,6 +0,0 @@
-# problem1.py
-
-def class_method(func):
-    def wrapper(cls, *args, **kwargs):
-        func(cls, *args, **kwargs)
-    return wrapper
--- Reinforcement_Exercises/RE_Seven/problem1.py
+++
@@ -1,5 +0,0 @@
-
-def partially_call(function, **kwargs):
-    def f(*n):
-        function(*n, **kwargs)
-    return f
